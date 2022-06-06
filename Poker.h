eac251f5c044045fbfc73c3790e916293fc42688
Ke1258 <101238484+Ke1258@users.noreply.github.com>
2022-06-06 17:31:57 +0800

    Add files via upload

--- Poker.h
+++ Poker.h
@@ -2,23 +2,25 @@
 #define POKER
 #include<string>
 #include<iostream>
+
 using namespace std;
-class Poker
+class Poker 
 {
 private:
     int poker[53];     //撲克
-    int pokerValue[53]; //撲克代表的數值
+    double pokerValue[53]; //撲克代表的數值
     string pokerName[53]; //撲克名
     int money;   //玩家錢數  
     int bet;     //玩家的賭注
 public:
     Poker();
     void inputBet();            //玩家輸入賭注
-    void newGame();
-    string getPokerF();  //用字串的形式返回玩家的牌
-    string getPokerL();  //用字串的形式返回莊家的牌
-    int getPorker();
-    int getPorkerValue(int);
+    int getmoney();
+    int getbet();
+    void setbet(int);
+    void setmoney(int);
+    int getPorker(int);
+    double getPorkerValue(int);
     string getPorkerName(int);
 };
 
